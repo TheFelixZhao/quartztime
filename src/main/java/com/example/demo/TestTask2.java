8969c348e13bea1c276b4e77996befdcff7c6083
TheFelixZhao <772523375@qq.com>
2019-06-13 14:20:35 +0800

    采用内存方式使用Quartz

---
+++ src/main/java/com/example/demo/TestTask2.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2019/6/13.
+ */
+public class TestTask2 extends QuartzJobBean {
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("TestQuartz02----" + sdf.format(new Date()));
+    }
+}
